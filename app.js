ab6d0c74bd6397e50397ee8e3dfebb4434a34e70
tlwr <tlornewr@gmail.com>
2014-07-19 21:44:53 +0000

    Imported angular and basic js file

---
+++ app.js
@@ -0,0 +1,10 @@
+angular.module("gown", ["gown.controllers", "gown.services"]).
+config([function() {}]);
+angular.module("gown.controllers", []).controller("intro", ["$scope", "$http", function($scope, $http) {
+    $scope.urlParams = [];
+    $scope.urlDone = false;
+    $scope.graph = "";
+    $scope.graphDone = false;
+    
+}]);
+angular.module("gown.services", []);
